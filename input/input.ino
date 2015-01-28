c500c3069b71c1d03d8f79acbdb6be09c6b829b8
Richard Fulop <developerfulop@gmail.com>
2015-01-28 11:16:33 -0500

    Signed-off-by: Richard Fulop <developerfulop@gmail.com>
    Fix input errors

--- input/input.ino
+++ input/input.ino
@@ -1,28 +1,31 @@
 int reloadIPin = 5;
-int fireIPin = 6;
-int firePin = 7;
+int fireButtonPin = 30;
+int firePin = 41;
+int ammo_vPin = 50;
 int clip_v = 6;
 const int clip_max = 6;
 
 void setup(){
   Serial.begin(9600);
   pinMode(reloadIPin, INPUT);
-  pinMode(fireIPin,   INPUT);
+  pinMode(fireButtonPin,   INPUT);
   pinMode(firePin,    OUTPUT);
-  pinMode(ammo_vPin, OUTPUT)
+  pinMode(ammo_vPin, OUTPUT);
+  
 }
 
 void loop(){
-  if (digitalRead(fireButtonPin)==HIGH && clip_v > 0){
+  if (digitalRead(fireButtonPin)==LOW){
+    Serial.println("Shot");
     digitalWrite(firePin, HIGH);
-    delay(500);
+    delay(1000);
     digitalWrite(firePin, LOW);
-    clip_v--;
+   // clip_v--;
   }
-  if (digitalRead(reloadButtonPin) == HIGH){
+  /*if (digitalRead(reloadButtonPin) == HIGH){
     clip_v = clip_max;
-  }
-  dispAmmo();
+  }*/
+  //dispAmmo();
 }
 
 void dispAmmo(){
