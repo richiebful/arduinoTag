b1b65ac84ad09fb184ab235570418f8874566bcb
Richard Fulop <developerfulop@gmail.com>
2015-03-19 10:42:01 -0400

    Display code push
    error fixes, as well

--- input/input.ino
+++ input/input.ino
@@ -11,28 +11,28 @@ void setup(){
   pinMode(fireButtonPin,   INPUT);
   pinMode(firePin,    OUTPUT);
   pinMode(ammo_vPin, OUTPUT);
-  
+
 }
 
 void loop(){
- dispAmmo ();
-  
+  dispAmmo ();
+
   if (digitalRead(fireButtonPin)== HIGH && clip_v > 0){
     while (digitalRead(fireButtonPin) == HIGH){}
     Serial.println("Shot");
     digitalWrite(firePin, LOW);
-     delay(100); 
+     delay(100);
     digitalWrite(firePin, HIGH);
     clip_v--;
   }
-  
+
   Serial.println(clip_v);
-  
+
   if (digitalRead(reloadButtonPin) == HIGH){
     while (digitalRead(reloadButtonPin) == HIGH){}
     clip_v = clip_max;
   }
-  
+
 }
 
 void dispAmmo(){
@@ -40,7 +40,6 @@ void dispAmmo(){
      digitalWrite(ammo_vPin,  HIGH);
   }
   else{
-     digitalWrite(ammo_vPin, LOW); 
-  } 
+     digitalWrite(ammo_vPin, LOW);
+  }
 }
-
--- oldWorkingCode/oldWorkingCode.ino
+++ oldWorkingCode/oldWorkingCode.ino
@@ -12,7 +12,7 @@ setup()
     for (int i = 0; i < 4; i++){
       pinMode(ledPins[i], OUTPUT);
     }
-    
+
     dispLife();
 }
 
@@ -21,53 +21,31 @@ loop()
 {
     unsigned int    total = 0;
     long            datum;
-    
-    
+
+
     for (int i = 0; i < 7; i++) {
-	datum = pulseIn(sensorPin, LOW);
-        Serial.println(datum);
-	total += datum;
+      datum = pulseIn(sensorPin, LOW);
+      Serial.println(datum);
+      total += datum;
     }
-    
+
     if (life_v <= 0){
-     life_v = 5; 
-    }  
-    
-    if (total / 7.0 > 100) {
-        Serial.println("Shot");
-	life_v--;
-        dispLife();
-        delay(500);
+      life_v = 5;
     }
-    // for (int i = 0; i < 20; i ++){
-    // total += bitStore[i];
-    // }
-    // Serial.println(total/20.0);
-    // if (total/20.0 < 2000 && total/20.0 > 1000){
-    // shot_f ++;
-    // }
-    // else{
-    // shot_f = 0;
-    // }
-    // if (shot_f > 2){
-    // Serial.println("Fire");
-    // }
+
+    if (total / 7.0 > 100) {
+      Serial.println("Shot");
+	    life_v--;
+      dispLife();
+      delay(500);
+    }
 }
 
 void dispLife(){
   for (int i = 0; i < life_v; i++){
-    digitalWrite(ledPins[i], HIGH); 
+    digitalWrite(ledPins[i], HIGH);
   }
   for (int j = life_v; j < 4; j++){
-    digitalWrite(ledPins[j], LOW); 
+    digitalWrite(ledPins[j], LOW);
   }
 }
-
-/*
- * void blinkLed(){ unsigned long currentMillis = millis();
- * if(currentMillis - previousMillis > blinkInterval) { // save the last
- * time you blinked the LED previousMillis = currentMillis; // if the LED
- * is off turn it on and vice-versa: if (ledState == LOW){ ledState =
- * HIGH; }else{ ledState = LOW; } // set the LED with the ledState of the
- * variable: digitalWrite(ledPin, ledState); } }
- */
