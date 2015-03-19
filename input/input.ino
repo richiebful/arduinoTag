8e7fe6aabe884529d900988ee9ba93bb8c58072f
Richard Fulop <developerfulop@gmail.com>
2015-03-19 11:19:18 -0400

    Add combined code

--- input/input.ino
+++ input/input.ino
@@ -1,28 +1,32 @@
-int reloadButtonPin = 53;
-int fireButtonPin = 46;
-int firePin = 43;
-int ammo_vPin = 50;
+int reloadButtonPin = 2;
+int fireButtonPin = 3;
+int firePin = 4;
+int ammo_vPin = 5;
 int clip_v = 6;
+int laser = 6;
 const int clip_max = 6;
 
 void setup(){
   Serial.begin(9600);
   pinMode(reloadButtonPin, INPUT);
   pinMode(fireButtonPin,   INPUT);
+  pinMode(laser, OUTPUT);
   pinMode(firePin,    OUTPUT);
   pinMode(ammo_vPin, OUTPUT);
   
 }
 
 void loop(){
- dispAmmo ();
+  dispAmmo ();
   
   if (digitalRead(fireButtonPin)== HIGH && clip_v > 0){
     while (digitalRead(fireButtonPin) == HIGH){}
     Serial.println("Shot");
-    digitalWrite(firePin, LOW);
-     delay(100); 
+    digitalWrite(firePin, LOW); 
+    digitalWrite(laser, HIGH);
+    delay(100); 
     digitalWrite(firePin, HIGH);
+    digitalWrite(laser, LOW);
     clip_v--;
   }
   
